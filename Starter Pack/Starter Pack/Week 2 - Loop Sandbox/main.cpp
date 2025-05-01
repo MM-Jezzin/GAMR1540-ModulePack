1c01e497ffd347df1f91246e785ffc6842f63628
MM-Jezzin <dalbyjoseph5@gmail.com>
2025-05-01 12:20:39 +0100

    Update loop

--- Starter Pack/Starter Pack/Week 2 - Loop Sandbox/main.cpp
+++ Starter Pack/Starter Pack/Week 2 - Loop Sandbox/main.cpp
@@ -4,6 +4,76 @@ using namespace std;
 
 int main()
 {
+	//Simple for loop
+	//for (int iCounter = 1; iCounter < 11; iCounter++) // Icounter equals 1, while iCounter is les than 10, add 1 to iCounter
+	//{
+	//	cout << iCounter << endl; //Print iCountet
+	//}
+
+
+
+	//Count controlled loop
+	
+	//int count;  //Making integer count
+	//count = 0;  // Making count equal 0
+
+	//while (count < 100) // While count is less than 100
+	//{
+	//	cout << count << endl; // Prints count, then ends the line
+	//	count++; // Adds one to count
+	//}
+	//cout << "Done" << endl; // Prints "done" after count is 100
+	
+
+	//While loop
+	// 
+	//int iCounter; //Making integer icount
+	//iCounter = 1; //Making iCounter equal 1
+	
+	//while (iCounter < 11) //While iCounter is less than 11
+	//{
+	//	cout << iCounter << endl; //Print iCounter
+	//	iCounter = iCounter + 1; //Add one to iCounter
+	//}
+
+	//Do-while loops
+
+	//char cInput;     // Declare variable 
+
+	//do 
+	//{
+	//	cout << "Enter a letter: ";    //Write to screen
+	//	cin >> cInput;                 // Get input
+	//} while (cInput != 'x');           // Test out condition
+	//cout << "You entered x, goodbye." << endl;   // Next Statement
+
+
+	int iNumber1;
+	int iNumber2;
+
+	cout << "Enter a number below 25: " << endl;
+	cin >> iNumber1;
+
+    cout << "Enter a number below 25: " << endl;
+	cin >> iNumber2;
+
+	while (iNumber1 > 24 or iNumber2 > 24) 
+	{
+		cout << "The numbers have to be less than 25, goodbye bozo!" << endl;
+		return 0;
+	}
+	for (int iNumber1 = 0; iNumber1 = iNu)
+
+	
+		
+	cout << "i\tj" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << i << "\t" << j << endl;
+		}
+	}
 
 	return 0;
 }
