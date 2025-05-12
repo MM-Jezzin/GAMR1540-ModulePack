d1f62d8dc3f99aef9fbd973e622caf6392149169
MM-Jezzin <dalbyjoseph5@gmail.com>
2025-05-12 20:21:09 +0100

    io - not finished

--- Starter Pack/Starter Pack/Week 3 - File IO/main.cpp
+++ Starter Pack/Starter Pack/Week 3 - File IO/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <cctype>
+using namespace std;
 
 int main()
 {
+    ifstream inputFile("text.txt");
+    ofstream outputFile("converted.txt");
+
+    char character;
+    while (inputFile.get(character)) 
+    {
+        outputFile.put(toupper(character));
+
+    }
+
     return 0;
 }
-
